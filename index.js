ffbf5263782a5be1eb196f5955b1abe3589c1e6e
Brian Pfeil <brian.pfeil@gmail.com>
2019-05-10 16:49:27 -0400

    added create resource group

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const msRestAzure = require("ms-rest-azure");
-const storageManagementClient = require("azure-arm-storage");
+const { ResourceManagementClient } = require("azure-arm-resource");
+const { StorageManagementClient } = require("azure-arm-storage");
 
 // get via `az account list`
 const subscriptionId = process.env.subscriptionId;
@@ -13,9 +14,31 @@ const run = async () => {
     process.env.domain
   );
 
-  const client = new storageManagementClient(credentials, subscriptionId);
+  const client = new StorageManagementClient(credentials, subscriptionId);
+
+  // create resource group
+  const resourceManagementClient = new ResourceManagementClient(
+    credentials,
+    subscriptionId
+  );
+  const exists = await resourceManagementClient.resourceGroups.checkExistence(
+    /* "cloud-shell-storage-eastus" */
+    "app"
+  );
+
+  const createResp = await resourceManagementClient.resourceGroups.createOrUpdate(
+    "my-test-group-01",
+    { location: "eastus" }
+  );
+
+  console.dir(createResp);
+
+  // create storage account
+  //client.storageAccounts.create()
+
   const accounts = await client.storageAccounts.list();
-  console.dir(accounts, { depth: null, colors: true });
+
+  //console.dir(accounts, { depth: null, colors: true });
 };
 
 run();
