f9ca75b8dee798fb4a7f3e8200b56b2419749da6
Brian Pfeil <brian.pfeil@gmail.com>
2019-05-10 20:14:45 -0400

    create resource group and storage account, then delete resource group

--- index.js
+++ index.js
@@ -4,41 +4,100 @@ const msRestAzure = require("ms-rest-azure");
 const { ResourceManagementClient } = require("azure-arm-resource");
 const { StorageManagementClient } = require("azure-arm-storage");
 
+// global.  its ok ... just for learning :)
+let credentials;
+
 // get via `az account list`
 const subscriptionId = process.env.subscriptionId;
 
-const run = async () => {
+const log = s => {
+  const outputString = typeof s === "string" ? s : JSON.stringify(s, null, 2);
+  console.log(outputString);
+};
+
+const generateRandomId = prefix => {
+  const self = generateRandomId;
+  self.exsitIds = self.exsitIds || {};
+  var randomId;
+  while (true) {
+    randomId = prefix + Math.floor(Math.random() * 10000);
+    if (!self.exsitIds || !(randomId in self.exsitIds)) {
+      break;
+    }
+  }
+  self.exsitIds[randomId] = true;
+  return randomId;
+};
+
+const getCredentials = async () => {
   const credentials = await msRestAzure.loginWithServicePrincipalSecret(
     process.env.clientId,
     process.env.secret,
     process.env.domain
   );
+  return credentials;
+};
 
+const setCredentials = async () => {
+  credentials = await getCredentials();
+};
+
+const resourceGroupExists = async resourceGroupName => {
+  const client = new ResourceManagementClient(credentials, subscriptionId);
+  const exists = await client.resourceGroups.checkExistence(resourceGroupName);
+  return exists;
+};
+
+const checkIfResourceGroupExists = async () => {
+  const resourceGroupName = `cloud-shell-storage-eastus`;
+  const exists = await resourceGroupExists(resourceGroupName);
+  log(`resource group "${resourceGroupName}" exists: ${exists}`);
+};
+
+const listStorageAccounts = async () => {
   const client = new StorageManagementClient(credentials, subscriptionId);
-
-  // create resource group
-  const resourceManagementClient = new ResourceManagementClient(
-    credentials,
-    subscriptionId
-  );
-  const exists = await resourceManagementClient.resourceGroups.checkExistence(
-    /* "cloud-shell-storage-eastus" */
-    "app"
-  );
-
-  const createResp = await resourceManagementClient.resourceGroups.createOrUpdate(
-    "my-test-group-01",
-    { location: "eastus" }
-  );
-
-  console.dir(createResp);
-
-  // create storage account
-  //client.storageAccounts.create()
-
   const accounts = await client.storageAccounts.list();
+  log(accounts);
+};
 
-  //console.dir(accounts, { depth: null, colors: true });
+const createStorageAccount = async (resourceGroupName, accountName) => {
+  const client = new StorageManagementClient(credentials, subscriptionId);
+  const resp = await client.storageAccounts.create(
+    resourceGroupName,
+    accountName,
+    { sku: { name: "Standard_LRS" }, kind: "StorageV2", location: "eastus" }
+  );
+  return resp;
+};
+
+const createResourceGroup = async (
+  name = "my-test-resource-group-01",
+  location = "eastus"
+) => {
+  const client = new ResourceManagementClient(credentials, subscriptionId);
+  const resp = await client.resourceGroups.createOrUpdate(name, {
+    location
+  });
+  return resp;
+};
+
+const deleteResourceGroup = async name => {
+  const client = new ResourceManagementClient(credentials, subscriptionId);
+  const resp = await client.resourceGroups.deleteMethod(name);
+  return resp;
+};
+
+const run = async () => {
+  await setCredentials();
+  const resourceGroupName = generateRandomId("auto");
+  log(`resourceGroupName=${resourceGroupName}`);
+  await resourceGroupExists(resourceGroupName);
+  await createResourceGroup(resourceGroupName);
+
+  const storageAccountName = generateRandomId("auto");
+  await createStorageAccount(resourceGroupName, storageAccountName);
+
+  await deleteResourceGroup(resourceGroupName);
 };
 
 run();
